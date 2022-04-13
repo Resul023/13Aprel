23faa2784ba095788370ce5f71f87d7894a020ff
Resul023 <73m6u7h@code.edu.az>
2022-04-12 20:43:00 -0700

    Home Work

---
+++ 13Aprel/13Aprel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ClassLibrary;
+namespace _13Aprel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meeting meeting1 = new Meeting();
+            meeting1.Name = "Met1";
+            meeting1.FromDate = new DateTime(2020, 03, 01);
+            meeting1.ToDate = new DateTime(2020, 03, 02);
+
+            Meeting meeting2 = new Meeting();
+            meeting2.Name = "Met2";
+            meeting2.FromDate = new DateTime(2020, 03, 03);
+            meeting2.ToDate = new DateTime(2020, 03, 04);
+
+            Meeting meeting3 = new Meeting();
+            meeting3.Name = "Met3";
+            meeting3.FromDate = new DateTime(2020, 03, 05);
+            meeting3.ToDate = new DateTime(2020, 03, 06);
+
+            MeetingSchedule newSchedule = new MeetingSchedule();
+            newSchedule.Mettings.Add(meeting1);
+            newSchedule.Mettings.Add(meeting2);
+            newSchedule.Mettings.Add(meeting3);
+            DateTime dateFrom = new DateTime(2020, 03, 03);
+            DateTime dateTo = new DateTime(2020, 03, 08);
+
+            newSchedule.SetMeeting("Met4", dateFrom, dateTo);
+            foreach (var item in newSchedule.Mettings)
+            {
+                Console.WriteLine(item);
+            }
+
+            /*DateTime check = new DateTime(2020, 03, 04);
+            Console.WriteLine(newSchedule.FindMeetingsCount(check));*/
+
+            /*Console.WriteLine(newSchedule.IsExistsMeetingByName("Met"));*/
+
+            /* foreach (var item in newSchedule.GetExistsMeetingByName("Met1"))
+             {
+                 Console.WriteLine(item);
+             }*/
+
+        }
+    }
+}
---
+++ 13Aprel/ClassLibrary/MeetingSchedule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class MeetingSchedule
+    {
+        public List<Meeting> Mettings = new List<Meeting>();
+        public void SetMeeting(string FullName, DateTime From, DateTime To)
+        {
+            int count = 0;
+            if (From<To)
+            {
+                foreach (var item in Mettings)
+                {
+                    if ((item.FromDate < From && item.ToDate < To && item.ToDate < From) || (item.FromDate > From && item.ToDate > To && item.ToDate > From) && From < To)
+                    {
+                        count++;
+
+                    }
+
+                }
+                if (count == Mettings.Count)
+                {
+
+                    Meeting newMetting = new Meeting();
+                    newMetting.Name = FullName;
+                    newMetting.FromDate = From;
+                    newMetting.ToDate = To;
+                    Mettings.Add(newMetting);
+                }
+                else
+                {
+                    throw new Exception("This schedule is Full");
+                }
+            }
+            else
+            {
+                Console.WriteLine("This type of schedule is not exist");
+            }
+  
+        }
+        public int FindMeetingsCount(DateTime Time) 
+        {
+            return Mettings.FindAll(x => x.FromDate > Time).Count;
+            
+        }
+        public bool IsExistsMeetingByName(string Name) 
+        {
+            return Mettings.Exists(x => x.Name == Name);
+        }
+        public List<Meeting> GetExistsMeetingByName(string Name) 
+        {
+            List<Meeting> NewList = new List<Meeting>();
+            foreach (var item in Mettings.FindAll(x => x.Name == Name))
+            {
+                NewList.Add(item);
+            }
+            return NewList;
+
+        }
+    }
+}
